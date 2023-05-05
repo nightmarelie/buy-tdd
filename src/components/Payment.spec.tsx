2e6dfc1f8f425cebca1c5d9c3e50a2ec6dcaad45
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-05-04 20:40:13 -0700

    feat: add more tests

--- src/components/Payment.spec.tsx
+++ src/components/Payment.spec.tsx
@@ -7,4 +7,21 @@ describe("Payment", () => {
 
     expect(screen.getByText("Payment")).toBeInTheDocument();
   });
+
+  it("shows the option of donate", () => {
+    render(<Payment amount={10} />);
+
+    expect(
+      screen.getByText("I would like to donate $1 to charity.")
+    ).toBeInTheDocument();
+  });
+
+  it("shows the text when the checkbox is checked", () => {
+    render(<Payment amount={10} />);
+
+    const checkbox = screen.getByRole("checkbox");
+    checkbox.click();
+
+    expect(screen.getByText("Thanks for your donation.")).toBeInTheDocument();
+  });
 });
