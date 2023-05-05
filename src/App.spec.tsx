9ff822fc6064c2b936df5279eef493316efdd00d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-05-04 20:24:45 -0700

    feat: add app and todo tests

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,54 +1,10 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { Todo } from "./components/Todo";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
 describe("App", () => {
-  it("renders an item", () => {
-    const todos = [
-      {
-        id: "id-1",
-        title: "buy some milk",
-      },
-    ];
+  it("renders a app", () => {
+    render(<App />);
 
-    render(<Todo items={todos} />);
-
-    expect(screen.getByText("buy some milk")).toBeInTheDocument();
-  });
-
-  it("renders multiple items", () => {
-    const todos = [
-      {
-        id: "id-1",
-        title: "buy some milk",
-      },
-      {
-        id: "id-2",
-        title: "buy some apples",
-      },
-    ];
-
-    render(<Todo items={todos} />);
-
-    expect(screen.getByText("buy some milk")).toBeInTheDocument();
-    expect(screen.getByText("buy some apples")).toBeInTheDocument();
-  });
-
-  it("add new item to the list", () => {
-    render(<Todo items={[]} />);
-
-    const input = screen.getByLabelText("input");
-    fireEvent.change(input, { target: { value: "buy some oranges" } });
-    fireEvent.keyDown(input, { key: "Enter", code: "Enter", charCode: 13 });
-
-    expect(screen.getByText("buy some oranges")).toBeInTheDocument();
-  });
-
-  it("marks an item as done", () => {
-    render(<Todo items={[{ id: "id-1", title: "buy some milk" }]} />);
-
-    const item = screen.getByText("buy some milk");
-    fireEvent.click(item);
-
-    expect(screen.queryByText("buy some milk")).not.toBeInTheDocument();
+    expect(screen.getByText("App")).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Todo } from "./components/Todo";
 function App() {
   return (
     <div className="App">
+      <h1>App</h1>
       <Todo />
     </div>
   );
