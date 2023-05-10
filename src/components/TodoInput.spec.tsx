8a638622510470ec4dc2892c0833ad47fede8813
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-05-09 23:55:30 -0700

    feat: add test

--- src/components/TodoInput.spec.tsx
+++ src/components/TodoInput.spec.tsx
@@ -8,4 +8,21 @@ describe("TodoInput", () => {
 
     expect(input).toBeInTheDocument();
   });
+
+  it("should call onItemAdded when enter is pressed", () => {
+    const onItemAdded = jest.fn();
+    render(<TodoInput onItemAdded={onItemAdded} />);
+    const input = screen.getByLabelText("input");
+
+    input.focus();
+    input.setSelectionRange(0, 0);
+    input.value = "Buy some milk.";
+    input.dispatchEvent(new KeyboardEvent("keydown", { key: "Enter" }));
+
+    expect(onItemAdded).toHaveBeenCalledTimes(1);
+    expect(onItemAdded).toHaveBeenCalledWith({
+      id: expect.any(String),
+      title: "Buy some milk.",
+    });
+  });
 });
