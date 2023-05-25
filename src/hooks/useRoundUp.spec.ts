93a5e92e1dc02b683ec4966b6671aceef6b786b3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2023-05-24 20:06:54 -0600

    test: add hook test

--- src/hooks/useRoundUp.spec.ts
+++ src/hooks/useRoundUp.spec.ts
@@ -2,11 +2,19 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useRoundUp } from "./useRoundUp";
 
 describe(`useRoundUp`, () => {
-  it(`should return roundUp, setRoundUp, roundUpValue, roundUpResult`, () => {
+  it(`should return total and tip`, () => {
     const { result } = renderHook(() => useRoundUp(0, false));
 
     const { total, tip } = result.current;
     expect(total).toEqual(0);
+    expect(tip).toEqual(1);
+  });
+
+  it(`should return total and tip when agreeToDonate is true`, () => {
+    const { result } = renderHook(() => useRoundUp(0, true));
+
+    const { total, tip } = result.current;
+    expect(total).toEqual(1);
     expect(tip).toEqual(1);
   });
 });
